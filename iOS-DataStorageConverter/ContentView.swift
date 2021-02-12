6575681254e6142e1b05072ffbf108b968ecaf62
yining wang <ywang@lcs.on.ca>
2021-02-12 11:59:31 -0500

    Added a picker for the From data storage unit.

--- iOS-DataStorageConverter/ContentView.swift
+++ iOS-DataStorageConverter/ContentView.swift
@@ -2,40 +2,40 @@
 //  ContentView.swift
 //  iOS-DataStorageConverter
 //
-//  Created by Yining Wang on 2021-02-12.
+//  Created by Russell Gordon on 2021-02-12.
 //
-
-
-
-
 
 import SwiftUI
 
 struct ContentView: View {
     
-    //MARK: Stored properties
-    
-    
+    // MARK: Stored properties
+    @State private var fromUnit: DataStorageUnit = .bit
     @State private var input: String = ""
-    
-    
-    //MARK: Computer properties
-    
-    private var output: String{
-        return"You typed in \(input)"
+
+    // MARK: Computed properties
+    private var output: String {
+        return "You typed in \(input) and selected \(fromUnit)"
     }
- 
-    
     
     var body: some View {
         
-        Form{
-            
-            TextField("e.g.: 1824", text: $input)
+        Form {
+            Picker("From unit:", selection: $fromUnit) {
+                Text(DataStorageUnit.bit.rawValue).tag(DataStorageUnit.bit)
+                Text(DataStorageUnit.nibble.rawValue).tag(DataStorageUnit.nibble)
+                Text(DataStorageUnit.byte.rawValue).tag(DataStorageUnit.byte)
+                Text(DataStorageUnit.kilobyte.rawValue).tag(DataStorageUnit.kilobyte)
+                Text(DataStorageUnit.megabyte.rawValue).tag(DataStorageUnit.megabyte)
+                Text(DataStorageUnit.gigabyte.rawValue).tag(DataStorageUnit.gigabyte)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            TextField("e.g.: 1024", text: $input)
                 .keyboardType(.numberPad)
-         Text(output)
+
+            Text(output)
         }
-        
     }
 }
 
