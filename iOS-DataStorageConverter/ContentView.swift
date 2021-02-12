2eb100feabbfb706e2355a8e9c793d9b58c1153f
yining wang <ywang@lcs.on.ca>
2021-02-12 11:12:09 -0500

    Added iOS target using SwiftUI framework to project

---
+++ iOS-DataStorageConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-DataStorageConverter
+//
+//  Created by Yining Wang on 2021-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-DataStorageConverter/iOS_DataStorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DataStorageConverterApp.swift
+//  iOS-DataStorageConverter
+//
+//  Created by Yining Wang on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DataStorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
